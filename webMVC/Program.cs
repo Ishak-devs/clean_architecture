ec30bc1b8e7fb0b3f84f3e8aa346b566408c9006
kc ishak <kouicicontact@yahoo.com>
2026-01-21 16:51:26 +0100

    almost ok

--- webMVC/Program.cs
+++ webMVC/Program.cs
@@ -1,11 +1,17 @@
 using Application.Service;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProduitRepository, IProduitRepository>();
+
 builder.Services.AddScoped<ProduitService>();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -16,13 +22,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Produit}/{action=Index}/{id?}");
 
 app.Run();
